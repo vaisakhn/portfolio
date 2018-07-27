28c38d643742e890ef4a015257b2ba18ff699dbb
vaisakhn <vaisakhn@qburst.com>
2018-07-27 19:06:44 +0530

    second commit

--- script/jq.js
+++ script/jq.js
@@ -11,13 +11,23 @@ function scrollFunction() {
 }
 
 
-
 $(document).ready(function() {
-    $(".education").hide();
-    $(".tech").hide();
-    $(".project").hide();
-    $(".person").hide();
-    $(".extra").hide();
+    $(".education, .tech, .project, .person, .extra").hide();
+    $("#fb").click(function() {
+        window.open("https://www.facebook.com/vaisakhnair/");
+    });
+    $("#ln").click(function() {
+        window.open("https://www.linkedin.com/vaisakhnair/");
+    });
+    $("#ln").click(function() {
+        window.open("https://www.twitter.com/vaisakhnair/");
+    });
+
+
+    $("#tabs").tabs();
+    $("#accordion").accordion();
+    $("#accordion2").accordion();
+    $("#resizable").resizable();
 
 
 
@@ -26,28 +36,44 @@ $(document).ready(function() {
     $(".myBtn").click(function() {
         $("html, body").animate({ scrollTop: 0 }, 3000);
     });
-    $("footer a").hover(function() {
-        $("footer a:hover").css("color", "black");
+    $("footer a").click(function() {
+
     });
 
     $("#eduLink").click(function() {
-        $("#education").css({ height: "1000px" });
-        $(".education").slideToggle(2000);
+        $("#eduLink").css("color", "black");
+        $(".education").show();
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".education").offset().top
+        }, 2000);
     });
     $("#techLink").click(function() {
-        $("#tech").css({ height: "620px" });
-        $(".tech").slideToggle(2000);
+        $(".tech").show();
+        $("#techLink").css("color", "black");
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".tech").offset().top
+        }, 2000);
     });
     $("#projectLink").click(function() {
-        $("#project").css({ height: "720px" });
-        $(".project").slideToggle(2000);
+        $(".project").show();
+        $("#projectLink").css("color", "black");
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".project").offset().top
+        }, 2000);
     });
     $("#personLink").click(function() {
-        $("#person").css({ height: "520px" });
-        $(".person").slideToggle(2000);
+        $(".person").show();
+        $("#personLink").css("color", "black");
+        $([document.documentElement, document.body]).animate({
+
+            scrollTop: $(".person").offset().top
+        }, 2000);
     });
     $("#extraLink").click(function() {
-        $("#extra").css({ height: "520px" });
-        $(".extra").slideToggle(2000);
+        $(".extra").show();
+        $("#extraLink").css("color", "black");
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".extra").offset().top
+        }, 2000);
     });
 });
